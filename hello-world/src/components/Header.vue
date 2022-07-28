4fbe0eca8e23b47412360d99d634410232f9e448
sarah <amiiitis_74@yahoo.com>
2022-07-28 13:32:23 +0430

    eslint removed from package json

---
+++ hello-world/src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "Header",
+    data() {
+        return {
+            message: "Hellooooo"
+        }
+    },
+}
+</script>
+
+<style scoped>
+.container{
+    margin: 2%;
+    color: rgb(13, 86, 135);
+}
+</style>
